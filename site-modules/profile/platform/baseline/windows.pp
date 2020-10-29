02590a4a13e8c9ae4db57d1d2508a4747edf1f3e
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-10-29 17:17:49 +0000

    sdsds

--- site-modules/profile/platform/baseline/windows.pp
+++ site-modules/profile/platform/baseline/windows.pp
@@ -1,14 +1,6 @@
 class profile::platform::baseline::windows {
 
-  # OS Specific
-  case $::windows_installation_type {
-    'Server': {
-      include::profile::platform::baseline::users::windows
-    }
-
-    'Client': {
-      include::profile::baseline::users::windows
-    }
-  }
+  include::profile::platform::baseline::users::windows
 
 }
+
