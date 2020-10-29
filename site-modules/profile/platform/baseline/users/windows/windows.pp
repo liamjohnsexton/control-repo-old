8a830e205d3f588a811fa2f4b8f961d57ff02dfa
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-10-29 16:48:00 +0000

    pleassse work

--- site-modules/profile/platform/baseline/users/windows/windows.pp
+++ site-modules/profile/platform/baseline/users/windows/windows.pp
@@ -1,9 +1,9 @@
 class profile::platform::baseline::users::windows {
 
   user { 'liam_sexton':
-    ensure   => present
-    password => 'Puppetlabs!'
-    groups   => ['Administrators']
+    ensure   => present,
+    password => 'Puppetlabs!',
+    groups   => ['Administrators'],
   }
 
 }
