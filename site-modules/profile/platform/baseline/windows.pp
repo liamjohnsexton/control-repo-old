ffd6ff8be400296365b0db4c99e490edac3e53c5
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-10-29 15:56:10 +0000

    comitting new code

---
+++ site-modules/profile/platform/baseline/windows.pp
@@ -0,0 +1,14 @@
+class profile::platform::baseline::windows {
+
+  # OS Specific
+  case $::windows_installation_type {
+    'Server': {
+      include::profile::platform::baseline::users::windows
+    }
+
+    'Client': {
+      include::profile::baseline::users::windows
+    }   
+  }
+
+}
