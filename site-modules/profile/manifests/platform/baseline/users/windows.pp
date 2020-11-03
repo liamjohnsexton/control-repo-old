4ea2807d17e40b998a75259940d43c26ed5f406c
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-03 14:12:06 +0000

    added number in to user resource password

--- site-modules/profile/manifests/platform/baseline/users/windows.pp
+++ site-modules/profile/manifests/platform/baseline/users/windows.pp
@@ -2,8 +2,8 @@ class profile::platform::baseline::users::windows {
 
   user { 'lsexton':
     ensure   => present,
-    password => 'Puppetlabs!',
-    groups   => 'Administrators',
+    password => 'Puppet4labs!',
+    groups   => ['Administrators'],
   }
 
 }
