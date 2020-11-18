751869d1c0b671d2726d367ac50d41f12cb98b98
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 17:00:58 +0000

    added chocolatey funcrtionality

--- site-modules/profile/manifests/platform/baseline/packages/packages.pp
+++ site-modules/profile/manifests/platform/baseline/packages/packages.pp
@@ -1,17 +1,22 @@
 class profile::platform::baseline::packages::packages{
 
-  archive { 'C:/Windows/Temp/7z.msi':
-    ensure => absent,
-    source => 'https://www.7-zip.org/a/7z1900-x64.msi',
-    user   => 0,
-    group  => 0,
-  }
+#doing it without chocolatey
+  #archive { 'C:/Windows/Temp/7z.msi':
+   #ensure => present,
+   #source => 'https://www.7-zip.org/a/7z1900-x64.msi',
+   #user   => 0,
+   #group  => 0,
+  #}
 
+  #class { 'archive':
+    #seven_zip_name     => '7-Zip New',
+    #seven_zip_source   => 'C:/Windows/Temp/7z.msi',
+    #seven_zip_provider => 'chocolatey',
+  #}
 
-  class { 'archive':
-    seven_zip_name     => '7-Zip New',
-    seven_zip_source   => 'C:/Windows/Temp/7z.msi',
-    seven_zip_provider => 'windows',
+#with chocolatey
+  package { '7zip':
+    ensure   => latest,
   }
 
 }
