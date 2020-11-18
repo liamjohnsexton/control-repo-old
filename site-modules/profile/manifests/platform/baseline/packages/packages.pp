1c74f173231bedfad104cc4a0fa983840e1c8b58
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 17:46:05 +0000

    added in reboot code

--- site-modules/profile/manifests/platform/baseline/packages/packages.pp
+++ site-modules/profile/manifests/platform/baseline/packages/packages.pp
@@ -4,8 +4,8 @@ class profile::platform::baseline::packages::packages{
   #archive { 'C:/Windows/Temp/7z.msi':
    #ensure => present,
    #source => 'https://www.7-zip.org/a/7z1900-x64.msi',
-   #user   => 0,
-   #group  => 0,
+   #user   => 'lsexton',
+   #group  => 'Administrators',
   #}
 
   #class { 'archive':
@@ -13,6 +13,8 @@ class profile::platform::baseline::packages::packages{
     #seven_zip_source   => 'C:/Windows/Temp/7z.msi',
     #seven_zip_provider => 'windows',
   #}
+
+
 
   #doing it with chocolatey
   case $operatingsystem {
@@ -31,6 +33,10 @@ class profile::platform::baseline::packages::packages{
 
   package { '7zip':
     ensure   => latest,
+  }
+
+  reboot { 'after':
+    subscribe       => Package['7zip'],
   }
 
 
