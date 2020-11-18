505ca0b1293c10bb3941036810999e130cf5dbdc
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 17:07:10 +0000

    added a version for package

--- site-modules/profile/manifests/platform/baseline/packages/packages.pp
+++ site-modules/profile/manifests/platform/baseline/packages/packages.pp
@@ -11,12 +11,12 @@ class profile::platform::baseline::packages::packages{
   #class { 'archive':
     #seven_zip_name     => '7-Zip New',
     #seven_zip_source   => 'C:/Windows/Temp/7z.msi',
-    #seven_zip_provider => 'chocolatey',
+    #seven_zip_provider => 'windows',
   #}
 
 #with chocolatey
   package { '7zip':
-    ensure   => latest,
+    ensure   => '19.0',
   }
 
 }
