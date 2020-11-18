f18012ee7fcacde0abf856dd79db95e048c5c5b8
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 16:45:58 +0000

    adding to packages

--- site-modules/profile/manifests/platform/baseline/packages/packages.pp
+++ site-modules/profile/manifests/platform/baseline/packages/packages.pp
@@ -1,8 +1,16 @@
 class profile::platform::baseline::packages::packages{
 
+  archive { 'C:/Windows/Temp/7z.msi':
+    ensure => present,
+    source => 'https://www.7-zip.org/a/7z1900-x64.msi',
+    user   => 0,
+    group  => 0,
+  }
+
+
   class { 'archive':
-    seven_zip_name     => '7-Zip 9.20 (x64 edition)',
-    seven_zip_source   => 'C:/Windows/Temp/7z920-x64.msi',
+    seven_zip_name     => '7-Zip New',
+    seven_zip_source   => 'C:/Windows/Temp/7z.msi',
     seven_zip_provider => 'windows',
   }
 
