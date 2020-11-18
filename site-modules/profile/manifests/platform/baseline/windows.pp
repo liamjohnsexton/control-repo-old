f00bf3c6c3e7c2803fc6a913760cfd9db28b5ff7
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 13:18:15 +0000

    added differentiation between vms

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -1,6 +1,14 @@
 class profile::platform::baseline::windows {
+#logic depending on the type of windows vm it is
+  case $::windows_product_name {
+    'Windows Server 2016 Standard':{
+      include ::profile::platform::baseline::users::windows
+      include ::profile::platform::baseline::directories::directories
+  }
 
-  include ::profile::platform::baseline::users::windows
-  include ::profile::platform::baseline::directories::directories
-
+#numpty liam has realised he's gotta spin up a VM with internet explorer lol
+    'Windows 10 Enterprise 2015 LTSB':{
+      include ::profile::platform::baseline::users::windows
+      include ::profile::platform::baseline::directories::directories
+  }
 }
