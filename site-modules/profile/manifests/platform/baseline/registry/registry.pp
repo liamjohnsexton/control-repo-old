5bdb948fd18c6523887fe96e6c587f0d2adef788
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 15:18:44 +0000

    added for local users this to be enabled too

--- site-modules/profile/manifests/platform/baseline/registry/registry.pp
+++ site-modules/profile/manifests/platform/baseline/registry/registry.pp
@@ -4,4 +4,8 @@ class profile::platform::baseline::registry::registry {
     ensure => present,
   }
 
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}':
+    ensure => present,
+  }
+
 }
