cf4a96ca3b6dcd4badbf35056437c352c324c462
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 15:35:26 +0000

    dsdds

--- site-modules/profile/manifests/platform/baseline/registry/registry.pp
+++ site-modules/profile/manifests/platform/baseline/registry/registry.pp
@@ -1,6 +1,6 @@
 class profile::platform::baseline::registry::registry {
 
-  registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     type   => dword,
     data   => 0x00000001
