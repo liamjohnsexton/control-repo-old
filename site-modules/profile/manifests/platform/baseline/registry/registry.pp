ef62ae1638bb13514b4b4f17f2adcc249c1f33a8
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-18 15:48:16 +0000

    added windows shut tracker enablement

--- site-modules/profile/manifests/platform/baseline/registry/registry.pp
+++ site-modules/profile/manifests/platform/baseline/registry/registry.pp
@@ -6,8 +6,16 @@ class profile::platform::baseline::registry::registry {
     data   => 0x00000001
   }
 
-  #registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}':
-    #ensure => present,
-  #}
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonOn':
+    ensure => present,
+    type   => dword,
+    data   => 0x00000001
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonUI':
+    ensure => present,
+    type   => dword,
+    data   => 0x00000001
+  }
 
 }
