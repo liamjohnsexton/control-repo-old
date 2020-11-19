63cd6e4ffd5d0c103451da281ab6516f1fcaa8d7
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-19 17:24:20 +0000

    dasdadsdas

--- site-modules/profile/manifests/platform/baseline/packages/packages.pp
+++ site-modules/profile/manifests/platform/baseline/packages/packages.pp
@@ -1,5 +1,26 @@
 class profile::platform::baseline::packages::packages {
 require ::chocolatey
+#doing it without chocolatey
+  #archive { 'C:/Windows/Temp/7z.msi':
+   #ensure => present,
+   #source => 'https://www.7-zip.org/a/7z1900-x64.msi',
+   #user   => 'lsexton',
+   #group  => 'Administrators',
+  #}
+
+  #class { 'archive':
+    #seven_zip_name     => '7-Zip New',
+    #seven_zip_source   => 'C:/Windows/Temp/7z.msi',
+    #seven_zip_provider => 'windows',
+  #}
+
+
+  #doing it with chocolatey
+  #class {'chocolatey':
+   # chocolatey_download_url         => 'https://chocolatey.org/api/v2/package/chocolatey',
+    #use_7zip                        => false,
+    #choco_install_timeout_seconds   => 2700,
+  #}
 
   package { '7zip':
     ensure    => latest,
