51a348a2c50f625f36ddead8ec64f94664297ddd
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-19 14:39:27 +0000

    updated case logic

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -1,7 +1,7 @@
 class profile::platform::baseline::windows {
 #logic depending on the type of windows vm it is
-  case $::windows_product_name {
-    'Windows Server 2016 Standard':{
+  case $::windows_installation_type {
+    'Server':{
       include ::profile::platform::baseline::users::windows
       include ::profile::platform::baseline::directories::directories
       include ::profile::platform::baseline::registry::registry
@@ -9,7 +9,7 @@ class profile::platform::baseline::windows {
   }
 
 #numpty liam has realised he's gotta spin up a VM with internet explorer lol
-    'Windows 10 Enterprise 2015 LTSB':{
+    'Client':{
       include ::profile::platform::baseline::users::windows
       include ::profile::platform::baseline::directories::directories
   }
