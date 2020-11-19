6e898aea4dd778d1317950848097868d97a3d1a5
Liam Sexton <liam.sexton@Liams-MacBook-Pro-2.local>
2020-11-19 16:52:55 +0000

    update baseline

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -1,16 +1,19 @@
 class profile::platform::baseline::windows {
 #logic depending on the type of windows vm it is
+
   case $::windows_installation_type {
     'Server':{
       include ::profile::platform::baseline::users::windows
       include ::profile::platform::baseline::directories::directories
       include ::profile::platform::baseline::registry::registry
       include ::profile::platform::baseline::iis::iis
+      include ::profile::platform::baseline::packages::packages
   }
 
 #numpty liam has realised he's gotta spin up a VM with internet explorer lol
     'Client':{
       include ::profile::platform::baseline::users::windows
       include ::profile::platform::baseline::directories::directories
+      include ::profile::platform::baseline::packages::packages
   }
 }
